1e8fca09a6370c6a7cad4559e52dac89bf082255
Gabriel Moura <gmoura.dev@outlook.com>
2026-09-08 18:50:22 -0300

    add main section mobile

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const openMenuButton = document.getElementById("open-menu-button");
+
+openMenuButton.addEventListener("click", () => {
+    const nav = document.getElementById("header-nav");
+    const iconeBotao = openMenuButton.querySelector("img");
+
+    nav.classList.toggle("mobile-aberto");
+
+    if(nav.classList.contains("mobile-aberto")) {
+        iconeBotao.src = "./assets/close-menu.png"
+        iconeBotao.className = "close-button"
+    } else {
+        iconeBotao.src = "./assets/mobile-menu.png"
+        iconeBotao.className = "open-button"
+    }
+})
