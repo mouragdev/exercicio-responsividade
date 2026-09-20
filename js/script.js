e1d4d6eaa7b473d06ad0e913009633613daecc46
Gabriel Moura <gmoura.dev@outlook.com>
2026-09-20 19:45:58 -0300

    fix comportamento nav menu mobile

--- js/script.js
+++ js/script.js
@@ -1,17 +1,13 @@
 const openMenuButton = document.getElementById("open-menu-button");
+const closeMenuButton = document.getElementById("close-menu-button");
 
-openMenuButton.addEventListener("click", () => {
-    const nav = document.getElementById("header-nav");
-    const iconeBotao = openMenuButton.querySelector("img");
+function openMenu() {
+    document.body.classList.add("menu-aberto");
+}
 
-    nav.classList.toggle("mobile-aberto");
-    document.body.classList.toggle("menu-aberto");
+function closeMenu() {
+    document.body.classList.remove("menu-aberto");
+}
 
-    if(nav.classList.contains("mobile-aberto")) {
-        iconeBotao.src = "./assets/close-menu.png"
-        iconeBotao.className = "close-button"
-    } else {
-        iconeBotao.src = "./assets/mobile-menu.png"
-        iconeBotao.className = "open-button"
-    }
-})
+openMenuButton.addEventListener("click", openMenu);
+closeMenuButton.addEventListener("click", closeMenu);
