ba5c57f6c5f573164bb86fbeec11553c0d811617
Gabriel Moura <gmoura.dev@outlook.com>
2026-09-21 05:21:00 -0300

    fixing header + manipulacao de formulario

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 const openMenuButton = document.getElementById("open-menu-button");
 const closeMenuButton = document.getElementById("close-menu-button");
 
+// 1. Selecionamos todos os links de navegação dentro do menu
+const navLinks = document.querySelectorAll("header nav ul li a");
+
 function openMenu() {
     document.body.classList.add("menu-aberto");
 }
@@ -11,3 +14,29 @@ function closeMenu() {
 
 openMenuButton.addEventListener("click", openMenu);
 closeMenuButton.addEventListener("click", closeMenu);
+
+// 2. Passamos por cada link e adicionamos um ouvinte de clique
+navLinks.forEach(link => {
+    link.addEventListener("click", closeMenu);
+});
+
+
+//  3. Salvamos os dados inseridos no formulario e previnimos o recarregamento da página
+document.getElementById("contact-form").addEventListener("submit", (event) => {
+    const nomeInformado = document.getElementById("nome-informado").value;
+    const emailInformado = document.getElementById("email-informado").value;
+    const mensagemFormulario = document.getElementById("mesage").value;
+    
+    event.preventDefault()
+    
+    console.log(`Nome: ${nomeInformado}`)
+    console.log(`Email: ${emailInformado}`)
+    console.log(`Mensagem: ${mensagemFormulario}`)
+
+    alert(`Obrigado ${nomeInformado}, sua mensagem foi enviada com sucesso.`)
+
+    document.getElementById("nome-informado").value = ""
+    document.getElementById("email-informado").value = ""
+    document.getElementById("mesage").value = ""
+    
+})
